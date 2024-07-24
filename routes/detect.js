46f7a0c8c4573498f3d20c40fdbcaffceaf5c72d
andres1000-7 <96263820+andres1000-7@users.noreply.github.com>
2024-07-24 19:50:56 -0400

    Object detection libraries now local. Comments added

--- routes/detect.js
+++ routes/detect.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+/**
+ * GET page.
+ * Renders the 'detect' view with the title 'Object Detection'.
+ */
 router.get('/', function (req, res, next) {
     res.render('detect', {title: 'Object Detection'});
 });
