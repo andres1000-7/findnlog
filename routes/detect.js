a4938189fbb9dcb4ffe25890ef54deb1fddd32da
andres1000-7 <96263820+andres1000-7@users.noreply.github.com>
2024-09-24 15:02:24 -0400

    detect route access handling

--- routes/detect.js
+++ routes/detect.js
@@ -1,7 +1,7 @@
 // routes/detect.js
 const express = require('express');
 const router = express.Router();
-const UserModel = require('../UserModel');
+const UserModel = require('../models/UserModel');
 
 router.get('/', async (req, res) => {
     if (!req.session.user) {
@@ -9,13 +9,23 @@ router.get('/', async (req, res) => {
         return;
     }
 
-    const cameFromWelcome = await UserModel.checkPreviousPage('/detect', '/welcome');
-    if (!cameFromWelcome) {
-        res.redirect('/welcome');
-        return;
-    }
+    const currentPage = '/detect';
+    const email = req.session.user.email;
+    const previousPage = req.session.currentPage;
 
-    res.render('detect', { title: 'Live Object Detection' });
+    const isVerified = await UserModel.verifyUser(email, currentPage, previousPage);
+    if (isVerified) {
+        req.session.currentPage = currentPage;
+        // const accessiblePages = await UserModel.getAccessiblePages(email);
+        res.render('detect', { user: req.session.user });
+    } else {
+        req.session.destroy(err => {
+            if (err) {
+                console.error('Error destroying session:', err);
+            }
+            res.redirect('/');
+        });
+    }
 });
 
 module.exports = router;
